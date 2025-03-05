45dd12f9225db8018bc435bd977f5a249c3fc48c
Daniela Castro <dcastrog1@eafit.edu.co>
2025-03-05 09:16:40 -0500

    Add DTOs and mappers for destination requests and responses

---
+++ Dtos/Response/DestinationResponse.cs
@@ -0,0 +1,8 @@
+namespace AmadeusG3_Neo_Tech_BackEnd.Dtos.Response
+{
+    public class DestinationResponse{
+
+        public string DestinoA { get; set; } = string.Empty;
+        public string DestinoE { get; set; } = string.Empty;
+    }
+}
